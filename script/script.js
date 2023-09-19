3134d66d8f1154e4869deb8c1ccc957addc1b9dc
Roger Anthony D. Bairoy <108564918+Poccho@users.noreply.github.com>
2023-09-19 08:39:54 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -27,3 +27,14 @@ function displayProd() {
 }
 
 document.getElementById('prodButton').addEventListener("click", displayProd);
+
+
+function displayDiv() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum / secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('divButton').addEventListener("click", displayDiv);
